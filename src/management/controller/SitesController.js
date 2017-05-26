f332619d043428b04ef6696265e8b66be0f4115a
Mathieu Maes <mathieu@zimmo.be>
2017-05-26 13:27:12 +0200

    Implement updateSite

--- src/management/controller/SitesController.js
+++ src/management/controller/SitesController.js
@@ -1,12 +1,14 @@
 import { Site } from '../../domain/site';
-const mapUserRole = (site, userId) => {
-    for (let user in site.users) {
+const getUserRole = (site, userId) => {
+    for (let user of site.users) {
         if (user.userId === userId) {
-            site.role = user.role;
-            break;
+            return user.role;
         }
     }
-    site.role = null;
+    return null;
+};
+const mapUserRole = (site, userId) => {
+    site.role = getUserRole(site, userId);
     return site;
 };
 
@@ -53,6 +55,35 @@ module.exports = () => {
                 });
             });
         },
+
+        /**
+         * @param req
+         * @param res
+         */
+        updateSite: (req, res) => {
+            const site = req.site;
+            const role = getUserRole(req.site, req.user.user_id);
+            console.log(req.site, req.user);
+            if (role !== 'OWNER') {
+                return res.status(403).send('you are not the owner', role);
+            }
+            if (req.body.name) {
+                site.set('name', req.body.name);
+            }
+            /**
+             * @todo some logic will be required to deal with the users attribue:
+             *  - An owner can't remove himself from the list, or change his role
+             *  - Only owners are allowed to update this list
+             */
+            site.save().then(() => {
+                res.status(200).send(mapUserRole(site.toJSON(), req.user.user_id));
+            }).catch(err => {
+                console.error(err);
+                res.status(500).send('error occured');
+            });
+
+        },
+
         removeSite: (req, res) => {
             req.site.remove()
                 .then(() => {
